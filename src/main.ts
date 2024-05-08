4d03bdd0acaf7060294961ec448661aa21a064f9
Mikhail Stankevich <mikhail.stankevich@sap.com>
2024-05-08 17:45:52 +0200

    feat: complete task 8

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,4 @@
-// import { NestFactory } from '@nestjs/core';
-//
-// import * as helmet from 'helmet';
-//
-// import { AppModule } from './app.module';
-//
-// const port = process.env.PORT || 4000;
-//
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//
-//   app.enableCors({
-//     origin: (req, callback) => callback(null, true),
-//   });
-//   app.use(helmet());
-//
-//   await app.listen(port);
-// }
-// bootstrap().then(() => {
-//   console.log('App is running on %s port', port);
-// });
-
-
 import { NestFactory } from '@nestjs/core';
-import helmet from 'helmet';
 import serverlessExpress from '@codegenie/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
@@ -31,16 +7,10 @@ let server: Handler;
 
 async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: (req, callback) => callback(null, true),
-  });
-  app.use(helmet());
-
+  app.enableCors();
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
-
   return serverlessExpress({ app: expressApp });
 }
 
